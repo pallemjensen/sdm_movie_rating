d049cdc56410d25dc672c55dc0561dabb86c1b9a
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2018-10-25 15:35:39 +0200

    Preliminary testing - 1 test working so far

--- SdmTest/SdmTest.cs
+++ SdmTest/SdmTest.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using sdm_movie_rating;
 
 namespace SdmTest
 {
@@ -8,6 +10,14 @@ namespace SdmTest
         [TestMethod]
         public void NumberOfReviewsFromNTest()
         {
+            int x = 1488844;
+
+            SdmLib sdmLib = new SdmLib("ratings.json");
+
+            // Assert.IsNotNull(sdmLib.List.ElementAt(0));
+
+            //Movie id at element 0 = 1488844
+            Assert.AreEqual(sdmLib.List.ElementAt(0).Movie, x);
 
         }
     }
