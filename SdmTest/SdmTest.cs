f3a986ea128f601da30e4fcdc67803a1759717c2
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2018-10-25 14:59:36 +0200

    made_test_class

---
+++ SdmTest/SdmTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SdmTest
+{
+    [TestClass]
+    public class SdmTest
+    {
+        [TestMethod]
+        public void NumberOfReviewsFromNTest()
+        {
+
+        }
+    }
+}
